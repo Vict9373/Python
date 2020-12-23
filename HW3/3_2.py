945b0a5a902fe0e057338b9a28767e1705428627
Vict9373 <vict1973@gmail.com>
2020-12-23 17:12:11 +0300

    Домашняя работа к уроку 3

---
+++ HW3/3_2.py
@@ -0,0 +1,5 @@
+def my_func(surname, name, year, city, email, phone):
+    print(surname, name, year, city, email, phone)
+
+
+my_func(surname="Анисимов", name="Виктор", year="1973", city="Москва", email="victor@mail.ru", phone="+79007775533")
